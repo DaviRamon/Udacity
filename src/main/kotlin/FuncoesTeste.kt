3292fe4c8d8a876a20013c684b6b709de51cc696
Davi Ramon <eudavi.ramon@gmail.com>
2021-05-08 21:07:56 -0300

    aula 3

--- src/main/kotlin/FuncoesTeste.kt
+++ src/main/kotlin/FuncoesTeste.kt
@@ -1,5 +1,17 @@
 fun main(args: Array<String>) {
 
+    /*
+    Crie uma função main() que receba um argumento representando a hora no formato de 24 horas (entre 0 e 23, incluindo esse dois valores).
+
+    Na função main(), verifique se a hora é anterior ao meio-dia (<12) e imprima "Good morning, Kotlin"; caso contrário, imprima "Good night, Kotlin".
+
+    Observações
+    Lembre-se que todos os argumentos da função main() são Strings, então você terá que converter este argumento para uma Int antes de aplicar a verificação.
+
+    Avançado
+    Tente usar os modelos de string do Kotlin para fazer isso em apenas uma linha.*/
+
+
     // como eu fiz
     val valor = "${args [0]}".toInt()
     if (valor < 12 ) println("Good Morning, Kotlin") else println("Good night, Kotlin")
