3764b2e9cf88f743ecaa6b87b14f209701714540
Davi Ramon <eudavi.ramon@gmail.com>
2021-05-10 16:52:38 -0300

    teste biscoito da sorte

--- src/main/kotlin/funcoes/BiscoitoDaSorteTeste.kt
+++ src/main/kotlin/funcoes/BiscoitoDaSorteTeste.kt
@@ -3,9 +3,9 @@ package funcoes
 import java.util.*
 
 fun main() {
-getFortuneCookie()
-
+    getFortuneCookie()
 }
+
 
 fun getBirthday(): String? {
     print("What is your birthday?  ")
@@ -41,15 +41,38 @@ fun getFortuneCookie() {
             do {
                 println("Enter a valid number: ")
             } while (convertBirthdayToInt() == null) // verifica se o valor digitado constinua sendo nulo.
-            println(randomFortune()) // caso o usuario digite um numero termina o laço e mostra a sorte do dia.
+            println("You fortune is: ${randomFortune()}") // caso o usuario digite um numero termina o laço e mostra a sorte do dia.
 
         }
-        else -> println (randomFortune())
+        else -> println("You fortune is: ${randomFortune()}")
 
     }
 
 }
 
+// RESPOSTA SUGERIDA PELO DESAFIO
+fun getFortune2() : String {
+    val fortunes = listOf("You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends, because they are your greatest fortune.")
+    print("\nEnter your birthday: ")
+    val birthday = readLine()?.toIntOrNull() ?: 1
+    return fortunes[birthday.rem(fortunes.size)]
+}
+
+
+fun mainsz(args: Array<String>) {
+    var fortune: String
+    for (i in 1..10) {
+        fortune = getFortune2()
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break
+    }
+}
 
 
 
